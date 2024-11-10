1bca6509f6c9e4dc706422cf361a7ef98a622f33
dam824 <damiendagory@gmail.com>
2024-11-10 19:59:57 +0100

    first commit

---
+++ bot.js
@@ -0,0 +1,40 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+//token du bot
+const token = "8024822737:AAEUMq3b4j_S2Pxw8TgiIfE07B89dTkdDnQ";
+
+//Creation instance bot
+const bot = new TelegramBot(token, { polling: true });
+
+//function rappel pick
+function sendReminder(chatId) {
+  bot.sendMessage(
+    chatId,
+    "N'oubliez pas de pick votre joueur au risque de la carotte 🥕!"
+  );
+}
+
+//demarer le rappel quotidien
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  //Welcom message
+  bot.sendMessage(
+    chatId,
+    "Bonjour, je vous rappelerai chaques jours de faire votre pick TTFL afin d'éviter la 🥕 "
+  );
+
+  //configuration rappel quotidien
+  const interval = setInterval(() => {
+    const now = new Date();
+
+    //configuration du rappel 12h et 18h
+    if (
+      (now.getHours() === 12 || now.getHours() === 18) &&
+      now.getMinutes() === 0
+    ) {
+      sendReminder(chatId);
+    }
+  }, 60000 );
+
+});
